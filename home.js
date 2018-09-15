0a551a696103acf5ae99028faeecb43c5069a6c1
Matt Williams <mwilliamsii233203@gmail.com>
2018-09-15 14:22:44 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -3,5 +3,31 @@
     
     function init(){
         $('.sidenav').sidenav();
+        $('#ied').on('click', ied);
+        $('#home').on('click', home);
+    }
+
+    function ied(){
+        $('.ied').css('display', 'block');
+        $('.info').css('display', 'none');
+        $('.compSci').css('display', 'none');
+        $('.about').css('display', 'none');
+
+        $('.sidenav').sidenav();
+        $('html, body').animate({
+            scrollTop: $('.ied').offset().top},
+        "slow");
+    }
+
+    function home(){
+        $('.ied').css('display', 'none');
+        $('.info').css('display', 'block');
+        $('.compSci').css('display', 'none');
+        $('.about').css('display', 'none');
+
+        $('.sidenav').sidenav();
+        $('html, body').animate({
+            scrollTop: $('.info').offset().top},
+        "slow");
     }
 })();
