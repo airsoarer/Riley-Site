4894a20c44beabab6ce6e470ff2e77b87e8e1d3f
Matt Williams <mwilliamsii233203@gmail.com>
2018-09-21 14:55:16 -0400

    Update home.js

--- home.js
+++ home.js
@@ -5,6 +5,7 @@
         $('.sidenav').sidenav();
         $('#ied').on('click', ied);
         $('#home').on('click', home);
+        $('#compSci').on('click', compSci);
     }
 
     function ied(){
@@ -28,6 +29,18 @@
         $('.sidenav').sidenav();
         $('html, body').animate({
             scrollTop: $('.home').offset().top},
+        "slow");
+    }
+    
+    function compSci(){
+        $('.ied').css('display', 'none');
+        $('.home').css('display', 'none');
+        $('.compSci').css('display', 'block');
+        $('.about').css('display', 'none');
+
+        $('.sidenav').sidenav();
+        $('html, body').animate({
+            scrollTop: $('.compSci').offset().top},
         "slow");
     }
 })();
